82b7690d768ce80e0ceafd4add4b4922e14564c7
cong qi <qi.co@northeastern.edu>
2024-06-22 19:29:54 +0800

    migrate from old repo

---
+++ src/components/layout/header.tsx
@@ -0,0 +1,75 @@
+import ThemeToggle from '@/components/layout/ThemeToggle/theme-toggle';
+import { cn } from '@/lib/utils';
+import { MobileSidebar } from './mobile-sidebar';
+import { UserNav } from './user-nav';
+import Link from 'next/link';
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  navigationMenuTriggerStyle
+} from '@/components/ui/navigation-menu';
+import { Input } from '@/components/ui/input';
+
+export default function Header() {
+  return (
+    <div className="supports-backdrop-blur:bg-background/60 fixed left-0 right-0 top-0 z-20 border-b bg-background/95 backdrop-blur">
+      <nav className="flex h-14 items-center justify-between px-4">
+        <div className="">
+          {/* <Link
+            href={'https://github.com/Kiranism/next-shadcn-dashboard-starter'}
+            target="_blank"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="mr-2 h-6 w-6"
+            >
+              <path d="M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3" />
+            </svg>
+          </Link> */}
+          <NavigationMenu>
+            <NavigationMenuList>
+              <NavigationMenuItem>
+                <Link href="/dashboard">
+                  <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                    Dashboard
+                  </NavigationMenuLink>
+                </Link>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <Link href="/dashboard/product">
+                  <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                    Products
+                  </NavigationMenuLink>
+                </Link>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <Link href="/dashboard/setting">
+                  <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                    Company Information
+                  </NavigationMenuLink>
+                </Link>
+              </NavigationMenuItem>
+            </NavigationMenuList>
+          </NavigationMenu>
+        </div>
+        {/* <div className={cn('block lg:!hidden')}>
+          <MobileSidebar />
+        </div> */}
+
+        <div className="flex items-center gap-2">
+          <Input className="w-96" type="search" placeholder="search..." />
+          <UserNav />
+          {/* <ThemeToggle /> */}
+        </div>
+      </nav>
+    </div>
+  );
+}
